dcadeb69139b7a96f79f71f1e5ccaf3090e0cfc8
Matej 'Yin' Gagyi <yinotaurus@gmail.com>
2014-08-04 21:28:39 +0200

    First argument to FizzBuzz is taken as number to count to, otherwise default is 30

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 
-# Create a algorithm, which counts from 1 to a given number max and prints the
+# Create a algorithm, which counts from 1 to a given number N and prints the
 # numbers with following exceptions: if the number is divisible by 3 print the
 # word 'Fizz' instead, if divisible by 5 print the word 'Buzz'. if divisible by
 # both, print both words in the given order.
 
-max = 30
+import sys
 
-for i in range(1, max + 1):
+N = 30
+if len(sys.argv) > 1:
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        pass
+
+for i in range(1, N + 1):
     out = ''
     if not i % 3:
         out += 'Fizz'

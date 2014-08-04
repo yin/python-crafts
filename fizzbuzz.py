28e2572eaaeb56f55eb28484a9bd376571def63c
Matej 'Yin' Gagyi <yinotaurus@gmail.com>
2014-08-04 21:12:12 +0200

    Added task description for FizzBuzz and made it executable.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python
+
+# Create a algorithm, which counts from 1 to a given number max and prints the
+# numbers with following exceptions: if the number is divisible by 3 print the
+# word 'Fizz' instead, if divisible by 5 print the word 'Buzz'. if divisible by
+# both, print both words in the given order.
+
 max = 30
 
 for i in range(1, max + 1):

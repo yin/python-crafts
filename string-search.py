4482df11b15ee600a09b79a5ef51fcf874697cb6
Matej 'Yin' Gagyi <yinotaurus@gmail.com>
2014-08-07 00:12:27 +0200

    Reworder string-search problem and gave nice intuition for KMP on a real table example

--- string-search.py
+++ string-search.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
-# Given word W and string S, find the first occurence of W in S. Return a tuple
-# (m, c), where m is the start index of the first match and c is the number of
-# comparisons you needed. If no match is found, then m should have value None.
-# If you compute any other information, which is interesting to inspect, return
-# it as the third element of the tuple: (m, c, i)
+# 1. Given word W and string S, find the first occurence of W in S. Return a
+# tuple (m, c), where m is the start index of the first match and c is the
+# number of comparisons you needed. If no match is found, then m should have
+# value None. If you compute any other information, which is interesting to
+# inspect, return it as the third element of the tuple: (m, c, i)
+# 2. Implement Knuth-Morris-Pratt aka KMP algorithm. Compute the kmp_table in a
+# separate function.
 
-test_sets = [
+search_test_sets = [
     {
         'word': 'abcd',
         'string': 'abcXabcd',
@@ -21,6 +23,18 @@ test_sets = [
         'word': 'ababX',
         'string': 'abababX',
         'results': [2],
+    },
+]
+kmp_table_test_sets = [
+    {
+        # This one server for KMP
+        'pattern': 'abcdababcdab',
+        'results': [-1, 0, 0, 0, 0, 1, 2, 1, 2, 3, 4, 5],
+    },
+    {
+        # This one server for KMP
+        'pattern': 'ababcdababcd',
+        'results': [-1, 0, 0, 1, 2, 0, 0, 1, 2, 3, 4, 5],
     },
 ]
 
@@ -69,7 +83,7 @@ def search(S, W):
 
 # In KMP algorithm, at every mismatch we try to skip previously matched
 # characters in S and in W. To do this we first build a table, let call it
-# overlay. This table will tell us, how many characters we can skip in W abd S
+# kmp_table. This table will tell us, how many characters we can skip in W abd S
 # when a mismatch happens at particular position in W.
 
 # For the example of S1 and W1, at first mismatch we can skip all characters
@@ -78,9 +92,12 @@ def search(S, W):
 # characters in S2 and also W2 and keep going matching only the last character
 # in W2.
 
-# To compute the overlay table each character of W with another 'candidate'
-# character of W, looking for repeating of the beginning of W inside of W.
-# More info http://en.wikipedia.org/wiki/Knuth%E2%80%93Morris%E2%80%93Pratt_algorithm
+# To compute the kmp_table compare each character of W with another
+# 'candidate' character of W, looking for repeating of the beginning of W
+# inside of W. Put the index of candidate in table at each match, at mismatch
+# reset candidate index to 0.
+# More info bellow, or at
+# http://en.wikipedia.org/wiki/Knuth%E2%80%93Morris%E2%80%93Pratt_algorithm
 def table_kmp(W):
     wlen = len(W)
     # We create an array [-1, 0, ...] of the length of W
@@ -108,15 +125,62 @@ def search_kmp(S, W):
 
 if __name__ == '__main__':
     i = 0
-    for test in test_sets:
+    # here we test the search functions
+    for test in search_test_sets:
         i = i + 1
         W = test['word']
         S = test['string']
         results = test['results']
         
-        print "=== Test %d ===" % (i)
+        print "=== String-search test %d ===" % (i)
         print "String S: {0} ({1})".format(S, len(S))
         print "Word W  : {0} ({1})".format(W, len(W))
         print "Expected results: {0}".format(results)
-        print "Straith-forward search: " + str(search(S, W))
-        print "Knuth-Morris-Prath search: " + str(search_kmp(S, W))
+        (index, comps) = search(S, W)
+        print "Straith-forward search: {0}, comparisons: {1}".format(index, comps)
+        (index, comps, kmp) = search_kmp(S, W)
+        print "Knuth-Morris-Prath search: {0}, comparisons: {1}".format(index, comps)
+        print ''
+
+    # Now let's test the KMP table function, it has more uses than than
+    # searching W in S, so it is interesting to see how it looks.
+    i = 0
+    for test in kmp_table_test_sets:
+        i = i + 1
+        P = test['pattern']
+        results = test['results']
+        # Here I format the pattern to fit the KMP tables nicelly
+        pretty_P = '[ ' + (', '.join(P)) + ']'
+
+        # After running this, we can see some nice patterns in the results
+        print "=== KMP table test %d ===" % (i)
+        print "Pattern         : {0} ({1})".format(pretty_P, len(P))
+        print "Expected results: {0}".format(results)
+        print "KMP table       : " + str(table_kmp(P))
+        print ''
+
+        # Here is an excerpt from output of the above:
+        # Pattern  : [ a, b, a, b, c, d, a, b, a, b, c, d] (12)
+        # KMP table: [-1, 0, 0, 1, 2, 0, 0, 1, 2, 3, 4, 5]
+
+        # Lets rotate the KMP table left:
+        # Pattern    : [ a, b, a, b, c, d, a, b, a, b, c,  d] (12)
+        # Shifted KMP: [ 0, 0, 1, 2, 0, 0, 1, 2, 3, 4, 5, -1]
+
+        # What we can see an overlap with the pattern now. It was not straigth-
+        # -forward at the beginning, but now we can see, what is the table_kmp
+        # computing: the number characters matching continously the beginning of
+        # the pattern. Every mismatch is denoted as 0 and each match increments
+        # the previous number in the table. At the end there is a special value,
+        # which marks the end of succesful matching.
+
+        # This is though not what we need to know in a string-matching algorithm
+        # in a practical situation. We already know that at every match we are
+        # supposed to increase our position in the word and in the string. What
+        # we want to know is, what to do, when we mismatch.
+
+        # So the original table tells us actually, how many character would have
+        # matched from beginning of the word W. So when we get a mismatch at
+        # S[m+i] and W[i], we can safely skip all characters in S, we already
+        # tried to match. And the table tells us, where to restart i to not miss
+        # any potential matches. Got it now?
